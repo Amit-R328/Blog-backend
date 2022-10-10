7366066f7d9c710ee44d3c05b07e4b0012029ce0
Amit <amitrozen2015@gmail.com>
2022-10-10 20:01:29 +0300

    added auth route and user route

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
+const authRoute = require("./api/routes/auth")
+const userRoute = require("./api/routes/users")
 
 dotenv.config()
+app.use(express.json())
 
 mongoose.connect(process.env.Mongo_url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(console.log("Connected to MONGO")).catch(err=>console.log(err))
+})
+.then(console.log("Connected to MONGO"))
+.catch(err=>console.log(err))
 
-app.listen("5000", () => {
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+
+app.listen("9000", () => {
     console.log("Backend is running")
 })
